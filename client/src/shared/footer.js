7b1be86f16bb9a9903a09fc4c7586e25013fba97
Charles <charleslanop@gmail.com>
2021-05-27 19:18:30 -0300

    fix(footer) update

--- client/src/shared/footer.js
+++ client/src/shared/footer.js
@@ -2,7 +2,7 @@ export const footer = () => {
     return `
         <footer class="footer mt-auto py-3 bg-light">
             <div class="container">
-                <span class="text-muted">Hello, welcome.</span>
+                <span class="text-muted">New Project &copy; 2021</span>
             </div>
         </footer>
     `;
