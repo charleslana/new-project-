2728a02f9a5ea39e0d7797d548dce539d66f03b1
Charles <charleslanop@gmail.com>
2021-05-26 23:13:48 -0300

    feat(design) update

--- client/src/pages/not-found.js
+++ client/src/pages/not-found.js
@@ -3,8 +3,22 @@ import { navBar } from '../shared/nav-bar.js';
 export const notFound = () => {
     return `
         ${navBar()}
-        <div class="container mt-5 pt-5">
-            <h1>Not Found</h1>
+        <div class="container mt-5 pt-5 text-center">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Oops!</h1>
+                    <h2>404 Not Found</h2>
+                    <p>
+                        Sorry, an error has occured, Requested page not found!
+                    </p>
+                    <div class="error-actions">
+                        <a href="/home" class="btn btn-primary click-action">
+                            <span class="glyphicon glyphicon-home"></span>
+                            Take Me Home
+                        </a>
+                    </div>
+                </div>
+            </div>
         </div>
     `;
 }
