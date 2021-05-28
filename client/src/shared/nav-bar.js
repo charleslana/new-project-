5226cb0f4239ebfc2362e94df9cfaff397f1cfc1
Charles <charleslanop@gmail.com>
2021-05-27 23:11:21 -0300

    feat(language) implementation

--- client/src/shared/nav-bar.js
+++ client/src/shared/nav-bar.js
@@ -6,7 +6,22 @@ export const navBar = () => {
                     aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <span class="navbar-brand mb-0 h1">New Project</span>
+                <div class="dropdown">
+                    <div class="language-pt" id="iconLanguage" data-bs-toggle="dropdown" aria-expanded="false">
+                    </div>
+                    <ul class="dropdown-menu" aria-labelledby="iconLanguage">
+                        <li class="dropdown-item change-language" data-language="pt">
+                            <div class="language-pt"></div>
+                        </li>
+                        <li class="dropdown-item change-language" data-language="en">
+                            <div class="language-en"></div>
+                        </li>
+                        <li class="dropdown-item change-language" data-language="es">
+                            <div class="language-es"></div>
+                        </li>
+                    </ul>
+                </div>
+                <span class="navbar-brand mb-0 h1 ms-2">New Project</span>
                 <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
                 <ul class="nav nav-pills ms-auto">
                     <li class="nav-item"><a href="/home" class="nav-link active click-action" id="home">Home</a></li>
